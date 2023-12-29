c4a5b26b40116a722b44a802301f82262fa2a363
ManviGumber <155147817+ManviGumber@users.noreply.github.com>
2023-12-29 04:59:05 -0500

    Update intro.js

--- basics/intro.js
+++ basics/intro.js
@@ -1,1 +1,7 @@
 console.log("hello world")
+const accountId = 144553
+// const is basically the constant  the value which we cannot change 
+// Variables :  containers 
+let account Email = "hitesh@google.com"
+var account Password "12345"
+accountcity = "Jaipur"
